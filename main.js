bc82fea3c352747bbccd0a826d4ae29d5f4da95c
ksheyon123 <52062612+ksheyon123@users.noreply.github.com>
2019-07-28 03:30:54 +0900

    nodejs-생활코딩_7/28

--- main.js
+++ main.js
@@ -2,25 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function (request, response) {
-    //request.url로 browser에 접근하는 url을 가져옴
-    var _url = request.url;
-
-    //url.parse(urlString[, parseQueryString[, slashesDenoteHost]]) url을 분석한다
-    var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    console.log(queryData.id);
-    if (_url == '/') {
-        var title = 'Welcome'
-    }
-    if (_url == '/favicon.ico') {
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`,'utf-8', function(err, description) {
-        var template = `
+function templateHTML(title, list, body) {
+    return `
     <!doctype html>
     <html>
     <head>
@@ -29,19 +12,63 @@ var app = http.createServer(function (request, response) {
     </head>
     <body>
         <h1><a href="/">WEB</a></h1>
-            <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
+        ${list}
+        ${body}
     </body>
     </html>
-    `
-    // fs.readFileSync(__dirname + url) 사용자가 접속한 url에 따라서  __dirname + url을 읽어옴
-    response.end(template);
-    });
+    `;
+}
+//filelist라는 매개변수가 정의가 되어 있지 않아서 filelist호출
+function templateList(filelist) {
+    var list = '<ul>';
+    var i = 0;
+    while (i < filelist.length) {
+        list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`
+        i = i + 1;
+    }
+    list = list + '</ul>';
+    return list;
+}
 
+var app = http.createServer(function (request, response) {
+    //request.url로 browser에 접근하는 url을 가져옴
+    var _url = request.url;
+
+    //url.parse(urlString[, parseQueryString[, slashesDenoteHost]]) url을 분석한다
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+
+    var fileFolder = './data';
+    fs.readdir(fileFolder, function (err, filelist) {
+
+    })
+
+    console.log(pathname);
+    console.log(url.parse(_url, true));
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            fs.readdir('./data', function (err, filelist) {
+                var title = 'Welcome';
+                var description = 'Hello Node.js'
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readdir('./data', function (err, filelist) {
+                fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
+                    var title = queryData.id;
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not Found');
+    }
 });
 app.listen(3000);
